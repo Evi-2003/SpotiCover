fb2369c30f67f2f0ddcb9dac5ed7cf0083f3b293
Remco Wammes <69012052+Remco17@users.noreply.github.com>
2020-11-05 22:00:00 +0100

    Everything is working right now.
    Took some time, but it's working right now. Only has one fix to go. Will add it to the to-do list.

--- run.py
+++ run.py
@@ -9,15 +9,21 @@ import spotipy.util as util
 from urllib.parse import urlparse
 from posixpath import basename, dirname
 import wget
+
+# maybe delete later
+from tkinter import *
+from PIL import ImageTk
+from PIL import Image
+import threading
 # We need some Authentication.
 # The Client_ID Client_Secret And Username needs to be set by yourself, for now.
-username = "-"
+username = "xw4dm4tsfaduaewklngslmilj"
 scope = "user-read-currently-playing"
 token = util.prompt_for_user_token(
     username,
     scope,
-    client_id='-',
-    client_secret='-',
+    client_id='75eb854eddec4ea0a3f02b6de123c2bc',
+    client_secret='d07026c49b1640139858a3ba7bbc3254',
     redirect_uri='http://example.com/callback/')
 
 
@@ -40,25 +46,35 @@ class SpotiCover(object):
             # Getting the Cover
             getImage = results["item"]["album"]["images"][0]['url']
             print(getImage)
-           # display(Image(filename=getImage))
+            # Downloads image without extension
             wget.download(getImage)
             parse_object = urlparse(getImage)
             path = parse_object.path
             coverID = basename(path)
-            print(coverID)
-
-
-
+            print(coverID)  # Printing out the name of the downloaded file.
             addJPG = coverID
             file = os.path.splitext(addJPG)[0]
-            os.rename(addJPG, file + ".jpg")
-
-        # DOES CURRENTLY OPEN WITH WINDOWS 10 PHOTO'S APP, NOT WHAT I WANTED... WILL LOOK INTO IT!
-            image = Image.open(coverID + '.jpg')
-            image.show()
-        # Looking when the song ends, and reload the script, and remove the downloaded image
+            os.rename(addJPG, file + ".jpg")  # Adding the .jpg extension to the extension-less file.
+            # Resizing to a better size
+            cover = coverID + ".jpg"
+            coverImages = Image.open(cover)
+            doingResize = coverImages.resize((300, 300))
+            doingResize.save('resized.jpg')
+            os.remove(cover)
+            # Showing the Cover
+            root = Tk()
+            canvas = Canvas(root, width=300, height=300)
+            canvas.pack()
+            coverIMG = ImageTk.PhotoImage(Image.open('resized.jpg'))
+            canvas.create_image(0,0,anchor=NW, image=coverIMG)
+            root.update()
+            findWindow = win32gui.FindWindow(None, "tk")
+            win32gui.MoveWindow(findWindow, -7, 0, 300, 300, True)
+            root.update()
+            # Looking when the song ends, and reload the script, and remove the downloaded image
             time.sleep(SpotifySongDuration - songTimeStamp)
-            os.remove(addJPG + ".jpg")
+            root.destroy()
+            os.remove('resized.jpg')
             os.system("reloader.py")
             # Restarting the script ones the video ends
             # We will need another script for this one
